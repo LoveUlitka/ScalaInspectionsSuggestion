6fbdbb3878e34191bc6b2ed85d6b2fb003473279
Anastasiia Chernievskaia <anastasiia.chernievskaia@macq.eu>
2023-08-21 15:57:53 +0200

    add more comments

--- src/main/scala/inspections/suggestion/DoHighlight.scala
+++ src/main/scala/inspections/suggestion/DoHighlight.scala
@@ -12,14 +12,17 @@ object DoHighlight {
 
   // Simple example of if (option.isDefined) option.get else defaultValue
   val example1: Unit = {
-    // initial code
+    // initial code (to be replaced)
     if (foo.bar.isDefined) update(foo.bar.get) else update(7)
 
     // suggestions
+    // 1
     foo.bar.fold(update(7))(update)
+    // 2
     foo.bar.map(update).getOrElse(update(7))
+    // 3
     update(foo.bar.getOrElse(7))
-
+    // 4
     foo.bar match {
       case Some(i) => update(i)
       case _ => update(7)
@@ -28,11 +31,13 @@ object DoHighlight {
 
   // Sometimes if doesn't have else block because it holds a side effect
   val example2: Unit = {
+    // initial code (to be replaced)
     if (foo.bar.isDefined) update(foo.bar.get)
 
     // suggestions
+    // 1
     foo.bar.map(update)
-
+    // 2
     foo.bar match {
       case Some(i) => update(i)
       case _ =>
@@ -43,17 +48,17 @@ object DoHighlight {
   val example3: Unit = {
     val values = List(0, 123)
 
-    // with AND
+    // initial code (to be replaced) with AND
     if (foo.bar.isDefined && values.contains(foo.bar.get)) update(6)
     // suggestions
     if (foo.bar.exists(values.contains)) update(6)
 
-    // with OR
+    // initial code (to be replaced) with OR
     if (foo.bar.isDefined || values.contains(foo.bar.get)) update(6)
     // suggestions
     if (foo.bar.forall(values.contains)) update(6)
 
-    // with additional option.get after the condition
+    // initial code (to be replaced) with additional option.get after the condition
     if (foo.bar.isDefined && values.contains(foo.bar.get)) update(foo.bar.get)
     // suggestions
     foo.bar.filter(values.contains).map(update)
@@ -63,17 +68,21 @@ object DoHighlight {
   val example4: Unit = {
     val defaultList: List[Int] = List(7)
 
+    // initial code (to be replaced)
     if (foo.bar.isDefined && listOpt.isDefined) defaultList ++ listOpt.get.map(_ * 10)
     else defaultList // it could be another action
 
     // suggestions
+    // 1
     foo.bar.zip(listOpt).map(defaultList ++ _._2).getOrElse(defaultList)
+    // 2
     foo.bar.map(_ => defaultList ++ listOpt.getOrElse(Nil))
+    // 3
     (foo.bar, listOpt) match {
       case (Some(_), Some(l)) => defaultList ++ l
       case _ => defaultList
     }
-
+    // 4
     // maybe a for comprehension? But it looks hard to insert into the code
     val res = for {
       _ <- foo.bar
@@ -83,17 +92,22 @@ object DoHighlight {
   }
 
   // Option to Future without using 3rd party libraries
+  // looks exactly like example1
   val example5: Unit = {
     implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
+    // initial code (to be replaced)
     if (foo.bar.isDefined) compute(foo.bar.get)
     else Future.successful(5)
 
     // suggestions
+    // 1
     foo.bar.fold(compute(5))(compute)
+    // 2
     foo.bar.map(compute).getOrElse(compute(5))
+    // 3
     compute(foo.bar.getOrElse(5))
-
+    // 4
     foo.bar match {
       case Some(i) => compute(i)
       case _ => compute(5)
